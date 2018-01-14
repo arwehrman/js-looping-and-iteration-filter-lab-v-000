06cdcf8266439561ef7b1b47d70f1c8061e86308
Amanda Wehrman <arwehrman@gmail.com>
2018-01-14 18:13:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function findMatching(drivers, string) {
   return drivers.filter(function(name){
     return name.toLowerCase() === string.toLowerCase()});
 }
+
+function fuzzyMatch(drivers, string){
+  
+}
